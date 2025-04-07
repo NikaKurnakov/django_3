80afb20aee9e3c0467fdd9392b134bab5b473736
NikaKurnakova <119730331+NikaKurnakov@users.noreply.github.com>
2025-04-07 16:29:47 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,15 +1,14 @@
 #Функция по исправлению оценок у определенного ребенка
 def fix_marks(schoolkid_name):
-	schoolkid = Schoolkid.objects.get_or_404(full_name__contains=schoolkid_name)
-	marks= Mark.objects.filter(schoolkid__full_name__icontains=schoolkid_name, points_range=["2","3"])
-	mark_fix = [mark_fix.save() for mark_fix in marks if setattr(mark_fix, 'points', "5")]
-	return mark_fix
+    schoolkid = get_object_or_404(Schoolkid, full_name__icontains=schoolkid_name)
+    updated_count = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+    return updated_count
 
 #Функция по создания комментария с похвалой от определенного учителя, предмета ребенку
 def create_commendation(schoolkid_name, subject, teacher):
-	schoolkid = Schoolkid.objects.get_or_404(full_name__contains=schoolkid_name)                                                                   
-	subject = Subject.objects.get_or_404(title__contains=subject)                                                                            
-	teacher = Teacher.objects.get_or_404(full_name__contains=teacher)                                                                                    
+	schoolkid = get_object_or_404(Schoolkid, full_name__contains=schoolkid_name)                                                                   
+	subject = get_object_or_404(Subject, title__contains=subject)                                                                            
+	teacher = get_object_or_404(Teacher, full_name__contains=teacher)                                                                                    
 	last_lesson = Lesson.objects.filter(
             subject=subject,
             teacher=teacher,
@@ -30,8 +29,8 @@ def create_commendation(schoolkid_name, subject, teacher):
         return commendation
 	
 #Функция по удалению плохих отметок
-def delete_bad_marks(schoolkid_name):
-        schoolkid = Schoolkid.objects.get_or_404(full_name__contains=schoolkid_name)
-        bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-        deleted_count = bad_marks.delete()[0]
-        return deleted_count
+def delete_chastisement(schoolkid_name):
+        schoolkid = get_object_or_404(Schoolkid, full_name__contains=schoolkid_name)
+        chastisement = Chastisement.objects.filter(schoolkid=schoolkid)
+        deleted_chastisement = chastisement.delete()
+        return deleted_chastisement
